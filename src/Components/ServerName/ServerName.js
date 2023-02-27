968e9766bf512687ee2cb2a4d8cab8e730eb0d0e
timothy-coder <nicopar666xd@gmail.com>
2023-02-27 12:11:36 -0500

    first commit

---
+++ src/Components/ServerName/ServerName.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, Title, ExpandIcon} from './ServerNameStyles'
+
+const ServerName = () => {
+  return (
+    <Container>
+        <Title>Acadimia DLine-code</Title>
+        <ExpandIcon/>
+    </Container>
+  )
+}
+
+export default ServerName
